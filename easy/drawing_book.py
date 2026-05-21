b064127d2884bc93ef954bc935e216441638e8bd
Jon Snow <jonsnow@Jons-MacBook-Air.local>
2026-05-21 08:52:40 +0700

    ref-logic: refactor efficient logic

--- easy/drawing_book.py
+++ easy/drawing_book.py
@@ -16,27 +16,10 @@ import sys
 #
 
 def pageCount(n, p):
-    tp = math.ceil(n/2)
-    if n%2 == 0:
-        tp += 1
-        
-    dp = math.ceil(p/2)
-    if p%2 == 0:
-        dp += 1
+    tp = math.ceil(n/2) + (n%2 == 0)
+    dp = math.ceil(p/2) + (p%2 == 0)
 
-    fs = 0
-    for s in range(1, (int(tp)+1)):
-        if s == dp:
-            fs = s-1
-            break
-        
-    fe = 0
-    for s in range((int(tp)+1), 1, -1):
-        if s == dp:
-            fe = tp-s
-            break
-
-    return fs if fs <= fe else fe
+    return min(dp-1, tp-dp)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
